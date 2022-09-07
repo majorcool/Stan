ca62359cb8390e2ad33ef6b1d09ce2727c076bf5
17354 <cheng.he.stu@2101.moonshotacademy.cn>
2022-09-07 16:19:36 +0800

    learning1.1.py

--- U1-L1/learning1.1.py
+++ U1-L1/learning1.1.py
@@ -8,8 +8,17 @@ price = 8.5
 weight = 7.5
 money = price*weight
 print(money)
+#买苹果-5元
+money = price*weight-5
+print(money)
 a=1
 print(type(a))
 a=1.75
 print(type(a))
+
+first_name="zhang"
+last_name="san"
+print(first_name+last_name)
+print("_"*50)
+
 
