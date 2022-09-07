c69b598bd1c90dcbe9b89ba1ef6d6669aa92c067
17354 <cheng.he.stu@2101.moonshotacademy.cn>
2022-09-07 19:36:51 +0800

    learning1.2.py

---
+++ U1-L1/learning1.2.py
@@ -0,0 +1,13 @@
+i = 10
+f = 10.5
+b = True
+print(i+f)
+print(i+b)
+print(f-b)
+print(i*f)
+first_name = "三"
+last_name = "张"
+print(first_name + last_name)
+print(first_name*10)
+print((last_name + first_name)*10)
+print(first_name + "10")
