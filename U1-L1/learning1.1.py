544d3e55b493e480fa0407fafc56db0a1a163e3b
17354 <cheng.he.stu@2101.moonshotacademy.cn>
2022-09-07 16:06:20 +0800

    learning1.1.py

---
+++ U1-L1/learning1.1.py
@@ -0,0 +1,15 @@
+a = 1
+qq_number = "1234567"  #定义一个变量记录QQ号码
+print(qq_number)
+qq_password = "123"  #定义一个变量记录QQ密码
+print(qq_password)
+#买苹果案例
+price = 8.5
+weight = 7.5
+money = price*weight
+print(money)
+a=1
+print(type(a))
+a=1.75
+print(type(a))
+
