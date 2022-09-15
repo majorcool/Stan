710e8c87e8a96c7eaf7095e29e307571ec5a52dd
17354 <cheng.he.stu@2101.moonshotacademy.cn>
2022-09-15 18:19:23 +0800

    hello world

---
+++ U1-L2/2.1.2.py
@@ -0,0 +1,11 @@
+alien_color="red"
+if alien_color=="green":
+    print("+10")
+else:
+    print("玩家因为射杀外星人获得了5points")
+
+alien_color="green"
+if alien_color=="green":
+    print("玩家获得了10points")
+else:
+    print("+5")
