2b85c8c33c44ce4fe9c949881d30652f9cf8d567
17354 <cheng.he.stu@2101.moonshotacademy.cn>
2022-09-15 18:22:28 +0800

    hello world

---
+++ U1-L2/2.1.3.py
@@ -0,0 +1,23 @@
+color="red"
+if color=="red":
+    print("+5")
+elif color=="green":
+    print("+10")
+else:
+    print("+15")
+
+color="green"
+if color=="red":
+    print("+5")
+elif color=="green":
+    print("+10")
+else:
+    print("+15")
+
+color="yellow"
+if color=="red":
+    print("+5")
+elif color=="green":
+    print("+10")
+else:
+    print("+15")
