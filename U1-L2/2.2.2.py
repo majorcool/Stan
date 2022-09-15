5e53e7a4182d1a2dcbbc003f7199679f3a036ea6
17354 <cheng.he.stu@2101.moonshotacademy.cn>
2022-09-15 19:17:17 +0800

    hello world

---
+++ U1-L2/2.2.2.py
@@ -0,0 +1,10 @@
+Student=False
+JianKangMa=3 #1:绿码 2:黄码 3:红码
+if Student==True:
+    print("看看健康码")
+    if JianKangMa==1:
+        print("可以进学校")
+    else:
+        print("不能进")
+else:
+    print("不是学生不能进")
