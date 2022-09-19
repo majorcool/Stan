3c525ed70a953794cb3a0aeb70afd489564ba2e5
17354 <cheng.he.stu@2101.moonshotacademy.cn>
2022-09-19 19:02:31 +0800

    hello world

--- U1-L3/3.1.4.py
+++ U1-L3/3.1.4.py
@@ -3,10 +3,10 @@ i=0
 guess=0
 while i<5:
     guess=int(input("请输入1-100之间的数字"))
-    if guess==87:
+    if guess==Num:
         print("猜对了")
         break
-    elif guess>87:
+    elif guess>Num:
         print("猜大了")
     else:
         print("猜小了")
