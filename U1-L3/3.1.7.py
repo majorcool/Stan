17bcc4f8627bad90b567229befd378a44675722e
17354 <cheng.he.stu@2101.moonshotacademy.cn>
2022-09-19 19:40:46 +0800

    hello world

---
+++ U1-L3/3.1.7.py
@@ -0,0 +1,6 @@
+i=0
+sum=0
+while i<100:
+    i+=1
+    sum=sum+i
+print(sum)
---
+++ U1-L3/3.1.8.py
@@ -0,0 +1,16 @@
+odd=0
+even=0
+sum=0
+i=1
+PanDing=0
+while i<101:
+    sum=sum+i
+    PanDing=i%2
+    if PanDing==0:
+        even=even+i
+    else:
+        odd=odd+i
+    i=i+1
+print(sum)
+print(odd)
+print(even)
