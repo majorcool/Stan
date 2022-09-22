783ad111e10f3ca5d27db8893c620166edbad3e5
17354 <cheng.he.stu@2101.moonshotacademy.cn>
2022-09-22 11:50:09 +0800

    hello world

---
+++ exam/exam2/1.py
@@ -0,0 +1,5 @@
+Name="XiaoMing"
+Sex="Male"
+Phone_Number="13486407748"
+Appearance="Good"
+Saving=100
---
+++ exam/exam2/2.py
@@ -0,0 +1,12 @@
+while(1):
+    Num1=float(input("输入数字一"))
+    Num2=float(input("输入数字二"))
+    YunSuanFangFa=int(input("选择运算方法:1.加2.减3.乘4.除"))
+    if YunSuanFangFa==1:
+        print(Num1+Num2)
+    elif YunSuanFangFa==2:
+        print(Num1-Num2)
+    elif YunSuanFangFa==3:
+        print(Num1*Num2)
+    else:
+        print(Num1/Num2)
