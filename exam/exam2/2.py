fee210b591d7d417a916e9259d807d7bf7d3bf0d
17354 <cheng.he.stu@2101.moonshotacademy.cn>
2022-09-22 11:58:30 +0800

    hello world

--- exam/exam2/2.py
+++ exam/exam2/2.py
@@ -2,11 +2,15 @@ while(1):
     Num1=float(input("输入数字一"))
     Num2=float(input("输入数字二"))
     YunSuanFangFa=int(input("选择运算方法:1.加2.减3.乘4.除"))
+    Chu=0
+    Cheng=0
     if YunSuanFangFa==1:
         print(Num1+Num2)
     elif YunSuanFangFa==2:
         print(Num1-Num2)
     elif YunSuanFangFa==3:
-        print(Num1*Num2)
+        Cheng=Num1*Num2
+        print("%f" % Cheng)
     else:
-        print(Num1/Num2)
+        Chu=Num1/Num2
+        print("%f" % Chu)
