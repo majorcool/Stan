df075bfe64937a744d00174ead49aa6706cc6d3f
17354 <cheng.he.stu@2101.moonshotacademy.cn>
2022-09-26 16:36:10 +0800

    hello world

--- U1-L3/(1).py
+++ U1-L3/(1).py
@@ -1,8 +1,13 @@
 grade=0
 sum=0
 ave=0
-for i in range (0,30):
-    grade=int(input("请输入成绩:"))
-    sum=sum+grade
+i=0
+while i<=30:
+    if grade<0 or grade>100:
+        print("请输入正确的值:")
+        continue
+    grade = int(input("请输入成绩:"))
+    sum = sum + grade
+    i=i+1
 ave=sum/30
 print("平均分为: %f" % ave)
