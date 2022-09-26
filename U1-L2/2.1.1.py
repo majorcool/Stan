63e106a6dbcd43d5cce996b760a4d21673567e9d
17354 <cheng.he.stu@2101.moonshotacademy.cn>
2022-09-26 17:03:22 +0800

    hello world

--- U1-L2/2.1.1.py
+++ U1-L2/2.1.1.py
@@ -1,11 +1,6 @@
 alien_color="green"
 if alien_color=="green":
     print("玩家获得了5points")
-else:
-    print("UNPASS")
-
 alien_color="red"
 if alien_color=="green":
     print("玩家获得了5points")
-else:
-    print("UNPASS")
