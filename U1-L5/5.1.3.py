1971b96249ee131034929cce342fb35e713de042
17354 <cheng.he.stu@2101.moonshotacademy.cn>
2022-09-29 10:54:15 +0800

    hello world

--- U1-L5/5.1.3.py
+++ U1-L5/5.1.3.py
@@ -3,9 +3,11 @@ list_num=[1,3,4,3,7,3,9,8,6,3]
 def num3():
     pos_3 = list_num.index(3)
     list_num.remove(3)
-    print(pos_3)
+    return pos_3
+a=0
 for i in range(0,list_num.count(3)):
-    num3()
+    a=num3()
+    print(a+i)
 print(list_num)
 
 
