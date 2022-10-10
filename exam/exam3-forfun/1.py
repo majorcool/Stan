e9b5a6dc626f5096e09365f535250178ac983828
17354 <cheng.he.stu@2101.moonshotacademy.cn>
2022-10-10 16:48:19 +0800

    hello world

--- exam/exam3-forfun/1.py
+++ exam/exam3-forfun/1.py
@@ -1,8 +1,8 @@
 def sum0(num_list):
     result=[]
     for i in range (0,len(num_list)-2):
-        for o in range (0,len(num_list)-1):
-            for p in range (0,len(num_list)):
+        for o in range (i+1,len(num_list)-1):
+            for p in range (o+1,len(num_list)):
                 x,y,z=num_list[i],num_list[o],num_list[p]
                 if x+y+z==0:
                     result.append(tuple(sorted((x,y,z))))
