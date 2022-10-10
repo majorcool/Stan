9f041c2b85eb7f9dca219d458a6e3d9ed6269ab3
17354 <cheng.he.stu@2101.moonshotacademy.cn>
2022-10-10 20:12:38 +0800

    hello world

--- U1-L4/4.3.5.py
+++ U1-L4/4.3.5.py
@@ -1,4 +1,6 @@
-'''def LX(h):
+def LX(h):
+    a=[]
+    b=[]
     Odd = True
     if h % 2 == 0:
         print("行数必须是奇数!!!")
@@ -6,9 +8,11 @@
 
     if Odd == True:
         for i in range(1, h // 2 + 2):
-            return(print(" " * (h - i), end=""),print("*" * (2 * i - 1)))
+            a.append(" " * (h - i))
+            a.append("*" * (2 * i - 1))
         for i in range(h // 2, 0, -1):
-            return(print(" " * (h - i), end=""),print("*" * (2 * i - 1)))
-print(LX(5))
-'''
-#太难不会
+            a.append(" " * (h - i))
+            a.append("*" * (2 * i - 1))
+    return(a)
+for i in range(0,len(LX(3))):
+    print(LX(3)[i],end="")
