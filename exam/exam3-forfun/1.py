8419125401f202b48de44f2ec32de7d82e61caa1
17354 <cheng.he.stu@2101.moonshotacademy.cn>
2022-10-10 16:26:54 +0800

    hello world

--- exam/exam3-forfun/1.py
+++ exam/exam3-forfun/1.py
@@ -1,18 +1,13 @@
 def sum0(num_list):
-    num_list=list(num_list)
-    temp_list=()
-    temp_list2=[]
-    count=0
-    for i in range(0,len(num_list)):
-        temp_list2.append(" ")
-    for i in range (0,len(num_list)):
-        for o in range (0,len(num_list)):
+    result=[]
+    for i in range (0,len(num_list)-2):
+        for o in range (0,len(num_list)-1):
             for p in range (0,len(num_list)):
-                if num_list[i]+num_list[o]+num_list[p]==0:
-                    temp_list=(num_list[i],num_list[o],num_list[p])
-                    temp_list2[i]=temp_list
-    print(set(temp_list2))#转换成数组后自动去处重复项
-sum0([1,2,3,4,5,-1,-2,-3,-4,-5])
+                x,y,z=num_list[i],num_list[o],num_list[p]
+                if x+y+z==0:
+                    result.append(tuple(sorted((x,y,z))))
+    return list(set(result))
+print(sum0([1,1,2,3,4,5,-1,-2,-3,-4,-5]))
 
 
 
