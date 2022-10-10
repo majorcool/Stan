f54e4130f225c9c0940bb420e4c2893ffc82c2f1
17354 <cheng.he.stu@2101.moonshotacademy.cn>
2022-10-10 20:14:52 +0800

    hello world

--- U1-L5/5.2.8.py
+++ U1-L5/5.2.8.py
@@ -1,14 +1,5 @@
 def repeat():
-    i=0
-    z=1
-    while i<=len(Sample):
-        z=i+1
-        while z<len(Sample):
-            if Sample[i]==Sample[z]:
-                Sample.pop(z)
-            z+=1
-        i+=1
-Sample=[1,2,6,7,9,10,10,9,3,4,1,2,3,4,8,8,8,8,8,8,8,88,8]
+    print(set(sorted(Sample)))
+Sample=[1,2,6,7,9,10,10,9,3,4,1,2,3,4,8,8,8,8,8,8,8,88,8,13,121,11]
 repeat()
-Sample=tuple(Sample)
-print(sorted(Sample))
+
