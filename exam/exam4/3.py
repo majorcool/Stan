008951a68df158fabd88618d365a461ac78d688e
17354 <cheng.he.stu@2101.moonshotacademy.cn>
2022-10-13 17:16:21 +0800

    hello world

--- exam/exam4/3.py
+++ exam/exam4/3.py
@@ -1,12 +1,8 @@
 def reverse(n,str='123456789'):
     count=len(str)//n+1
     temp=[]
-    for i in range(0,count):
-        if i==0:
-            temp.append(str[])
-        else:
-            temp.append(str[n*(i+1):n*i:-1])
-
+    for i in range(1,count+1):
+        temp.append(str[(n*i)-1:(n*(i-1))-1:-1])
     print(temp)
 
 reverse(3)
