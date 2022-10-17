1ae3eeb90524a3244b89deb9f9a7042696b1510a
17354 <cheng.he.stu@2101.moonshotacademy.cn>
2022-10-17 18:21:35 +0800

    hello world

--- exam/exam4/1.py
+++ exam/exam4/1.py
@@ -1,18 +1,9 @@
 def pattern(x,list_input=[],direction='right'):
-    temp=[]
     if direction!= 'right' and direction!='left':
         print("direction只能是left或者right")
         return False
     if direction=='right':
-        lens=len(list_input)
-        temp.append(list_input[-x:lens])
-        temp.append(list_input[0:-x])
-        temp[0].extend(temp[1])
-        print(temp[0])
+        print(list_input[x+1::]+list_input[:x+1:])
     if direction=='left':
-        lens=len(list_input)
-        temp.append(list_input[x:lens])
-        temp.append(list_input[0:x])
-        temp[0].extend(temp[1])
-        print(temp[0])
+        print(list_input[x::]+list_input[:x:])
 pattern(2,[1,2,3,4,5],'left')
