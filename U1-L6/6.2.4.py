56ebff8ab27ea3465ac5cb76f893a5ed2951ba68
17354 <cheng.he.stu@2101.moonshotacademy.cn>
2022-10-18 20:29:29 +0800

    hello world

--- U1-L6/6.2.4.py
+++ U1-L6/6.2.4.py
@@ -1,4 +1,42 @@
 def trans(num):
-    intab=1,5,10,50,100,500,1000
-    outab="I","V","X","L","C","D","M"
-    transtable=str.maketrans(intab,outab)
+    temp=[]
+    stri=""
+    roman=[1,4,5,9,10,40,50,90,100,400,500,900,1000]
+    for i in range (len(roman)-1,-1,-1):
+        while 1:
+            if num//roman[i]>0:
+                temp.append(roman[i])
+                num-=roman[i]
+            else:
+                break
+    for item in temp:
+        if item==1:
+            temp[temp.index(item)]="I"
+        if item==4:
+            temp[temp.index(item)]="IV"
+        if item==5:
+            temp[temp.index(item)]="V"
+        if item==9:
+            temp[temp.index(item)]="IX"
+        if item==10:
+            temp[temp.index(item)]="X"
+        if item==40:
+            temp[temp.index(item)]="XL"
+        if item==50:
+            temp[temp.index(item)]="L"
+        if item==90:
+            temp[temp.index(item)]="XC"
+        if item==100:
+            temp[temp.index(item)]="C"
+        if item==400:
+            temp[temp.index(item)]="CD"
+        if item==500:
+            temp[temp.index(item)]="D"
+        if item==900:
+            temp[temp.index(item)]="CM"
+        if item==1000:
+            temp[temp.index(item)]="M"
+    for str in temp:
+        stri+=str
+    print(stri)
+trans(2001)
---
+++ U1-L6/6.2.6.py
@@ -0,0 +1,19 @@
+def findlist(num):
+    count=1
+    count2=0
+    temp_str=''
+    num2=num
+    div=0
+    alphabet='ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    while 1:
+        if num//26**count>1:
+            count+=1
+        else:break
+    if count>1:
+        count-=1
+    for i in range (count,-1,-1):
+        temp_str+=alphabet[num//(26**i)-1]
+        div=num//(26**i)
+        num=num-div*(26**i)
+    print(temp_str)
+findlist(2147483647)
