e21825f177fbc6a7a7f2f22e62cee0fac45be681
17354 <cheng.he.stu@2101.moonshotacademy.cn>
2022-10-20 00:15:23 +0800

    hello world

---
+++ U1-L7/7.1.2.py
@@ -0,0 +1,21 @@
+'''def count():
+    count+=1
+    怎么限制递归次数啊啊啊啊
+'''
+def hahanum(n):
+    n=str(n)
+    temp=[]
+    sum=0
+    for char in n:
+        temp.append(int(char))
+    for items in range(0,len(temp)):
+        if items==0:
+            continue
+        temp[items]**=2
+        sum+=temp[items]
+    if sum==1:
+        print("是蛤蛤数")
+        return True
+
+    return hahanum(sum)
+hahanum(19)
