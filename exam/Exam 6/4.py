3b46ecbce56c289f3da726c7b39e10102fd04bc9
17354 <cheng.he.stu@2101.moonshotacademy.cn>
2022-11-07 16:58:24 +0800

    hello world

--- exam/Exam 6/4.py
+++ exam/Exam 6/4.py
@@ -1,12 +1,12 @@
 def max_sec_average(l,n):
     count=len(l)-n
     sum=0
-    ave=0
+    final=0
     for i in range (0,count):
         for z in range(i,i+n):
             sum+=l[z]
-        if ave<sum/n:
-            ave=sum/n
+        if final<sum:
+            final=sum
         sum=0
-    print(ave)
+    print(final/n)
 max_sec_average([1,12,-5,-6,50,3],4)
