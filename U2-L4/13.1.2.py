2710d653babf79a3e18d5a7495128a151ac40786
17354 <cheng.he.stu@2101.moonshotacademy.cn>
2022-11-08 23:24:34 +0800

    hello world

--- U2-L4/13.1.2.py
+++ U2-L4/13.1.2.py
@@ -1,9 +1,13 @@
 class Exam:
-    def __init__(self,id,start_time,end_time,points):
-        self.__start_time=start_time
-        self.__end_time=end_time
-        self.points=points
-        self.id=id
+    def __init__(self):
+        self.__start_time='5:00'
+        self.__end_time='7:00'
+        self.points=21
+        self.id=8
 class Test(Exam):
     def __init__(self):
+        super().__init__()
         self.points=10
+        print(self.points)
+        print(self.id)
+b=Test()
