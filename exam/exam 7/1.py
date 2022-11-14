a54b009128e36e4d9f9fe353fa6e3088655fdf05
17354 <cheng.he.stu@2101.moonshotacademy.cn>
2022-11-15 00:17:27 +0800

    hello world

--- exam/exam 7/1.py
+++ exam/exam 7/1.py
@@ -40,3 +40,40 @@ def gcd_pro_max_2(n):
         return gcd_pro_max_2(n[count+1])
 gcd_pro_max_2([1,2,3,4,5])
 '''
+class Animal():
+    def __init__(self):
+        self.health=100
+class Manager(Animal):
+    def perform(self):
+        self.health-=20
+    def __inspect(self,worker):
+        self.worker=worker
+        self.worker.performance=100
+    def feed(self,):
+        self.health+=20
+class Keeper(Manager):
+    def __init__(self):
+        super().__init__()
+        self.performance=100
+        if self.health<80:
+            self.performance-=20
+    def perform(self):
+        return False
+class Trainer(Manager):
+    def __init__(self):
+        super().__init__()
+        self.performance=100
+    def feed(self):
+        return False
+def everyday():
+    keeper=Keeper()
+    trainer=Trainer()
+    keeper.feed()
+    trainer.perform()
+    manager=Manager()
+    manager._Manager__inspect(keeper)
+    manager._Manager__inspect(trainer)
+
+
+
+
